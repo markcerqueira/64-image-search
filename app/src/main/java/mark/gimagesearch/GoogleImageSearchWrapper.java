7b29b035f1e0ceb7b53a8f5a14a2650161a43ff1
Mark Cerqueira <mark.cerqueira@gmail.com>
2014-10-12 23:14:04 -0700

    First pass - app makes call to API using search term "herro world," gets the response and parses it returning the object to the caller

---
+++ app/src/main/java/mark/gimagesearch/GoogleImageSearchWrapper.java
@@ -0,0 +1,57 @@
+package mark.gimagesearch;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+import mark.gimagesearch.models.ImageSearchResponse;
+
+public class GoogleImageSearchWrapper {
+    private static final String TAG = GoogleImageSearchWrapper.class.getName();
+
+    public interface GoogleImageSearchCallbackInterface {
+        void imageSearchResponseReceived(ImageSearchResponse imageSearchResponse);
+    }
+
+    public static void fetchImages(final String searchTerm, final GoogleImageSearchCallbackInterface callbackListener) {
+        NetworkUtils.runInThreadPool(new Runnable() {
+            @Override
+            public void run() {
+                StringBuilder builder = new StringBuilder();
+
+                try {
+                    URL url = new URL("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + URLEncoder.encode(searchTerm, "UTF-8"));
+                    URLConnection connection = url.openConnection();
+                    connection.addRequestProperty("Referer", "www.mark.gg");
+
+                    String line;
+                    InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
+                    BufferedReader reader = new BufferedReader(inputStreamReader);
+                    while ((line = reader.readLine()) != null) {
+                        builder.append(line);
+                    }
+
+                    Log.i(TAG, "fetchImages - response received: " + builder.toString());
+                } catch(Exception e) {
+                    Log.e(TAG, "fetchImages - exception thrown: " + e.getStackTrace());
+
+                    if(callbackListener != null) {
+                        callbackListener.imageSearchResponseReceived(null);
+                    }
+                }
+
+                ImageSearchResponse imageSearchResponse = JsonUtils.parseJson(builder.toString(), ImageSearchResponse.class);
+
+                Log.i(TAG, "fetchImages - ImageSearchResponse object created");
+
+                if(callbackListener != null) {
+                    callbackListener.imageSearchResponseReceived(imageSearchResponse);
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/mark/gimagesearch/MainActivity.java
+++ app/src/main/java/mark/gimagesearch/MainActivity.java
@@ -2,18 +2,32 @@ package mark.gimagesearch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import mark.gimagesearch.models.ImageSearchResponse;
 
 public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getName();
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        fetchImages();
     }
 
+    private void fetchImages() {
+        Log.i(TAG, "fetchImages - called");
+
+        GoogleImageSearchWrapper.fetchImages("herro world", new GoogleImageSearchWrapper.GoogleImageSearchCallbackInterface() {
+            @Override
+            public void imageSearchResponseReceived(ImageSearchResponse imageSearchResponse) {
+                Log.i(TAG, "fetchImages - called back with " + imageSearchResponse.responseData.resultsList.size() + " images");
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
