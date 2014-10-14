de787d5d8c5d5a779b4a189765cfd37380e7d59d
Mark Cerqueira <mark.cerqueira@gmail.com>
2014-10-14 00:30:35 -0700

    Extract network calling functionality from GoogleImageSearchAPI into NetworkUtils

--- app/src/main/java/mark/gimagesearch/GoogleImageSearchAPI.java
+++ app/src/main/java/mark/gimagesearch/GoogleImageSearchAPI.java
@@ -2,10 +2,7 @@ package mark.gimagesearch;
 
 import android.util.Log;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
 
 import mark.gimagesearch.models.ImageSearchResponse;
@@ -24,52 +21,48 @@ public class GoogleImageSearchAPI {
     }
 
     public static void fetchImages(final String searchTerm, final GoogleImageList googleImageList, final GoogleImageSearchCallbackInterface callbackListener) {
+        // we REQUIRE that callbackListener be non-null
+        if (callbackListener == null) {
+            Log.e(TAG, "fetchImages - callbackListener CANNOT be null");
+            return;
+        }
+
         NetworkUtils.runInThreadPool(new Runnable() {
             @Override
             public void run() {
-                StringBuilder builder = new StringBuilder();
+                int page = (googleImageList == null ? 0 : googleImageList.getNextPageIndex()) * 4;
 
+                // TODO - error if pages is > 60
+
+                URL url = null;
                 try {
-                    int page = (googleImageList == null ? 0 : googleImageList.getNextPageIndex()) * 4;
-
-                    // TODO - error if pages is > 60
-
-                    URL url = new URL("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + URLEncoder.encode(searchTerm, "UTF-8") + "&start=" + URLEncoder.encode(page + "", "UTF-8"));
-
-                    Log.i(TAG, "fetchImages - request url is: " + url.toString());
-
-                    URLConnection connection = url.openConnection();
-                    connection.addRequestProperty("Referer", "www.mark.gg");
-
-                    String line;
-                    InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
-                    BufferedReader reader = new BufferedReader(inputStreamReader);
-                    while ((line = reader.readLine()) != null) {
-                        builder.append(line);
-                    }
-
-                    Log.i(TAG, "fetchImages - response received: " + builder.toString());
+                    url = new URL("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + URLEncoder.encode(searchTerm, "UTF-8") + "&start=" + URLEncoder.encode(page + "", "UTF-8"));
                 } catch (Exception e) {
-                    Log.e(TAG, "fetchImages - exception thrown: " + e);
-
-                    if (callbackListener != null) {
-                        callbackListener.imageSearchResponseReceived(null);
-                    }
+                    Log.e(TAG, "fetchImages - exception thrown when constructing url: " + e);
+                    callbackListener.imageSearchResponseReceived(null);
                 }
 
-                ImageSearchResponse imageSearchResponse = JsonUtils.parseJson(builder.toString(), ImageSearchResponse.class);
+                Log.i(TAG, "fetchImages - request url is: " + url.toString());
 
-                Log.i(TAG, "fetchImages - ImageSearchResponse object created");
+                // make the network call
+                String networkResponse = NetworkUtils.callUrl(url);
 
-                if (callbackListener != null) {
-                    if(googleImageList == null) {
-                        callbackListener.imageSearchResponseReceived(new GoogleImageList(imageSearchResponse));
-                    } else {
-                        GoogleImageList newGoogleImageList = googleImageList;
-                        newGoogleImageList.appendImageUrls(imageSearchResponse);
+                // parse network response into an ImageSearchResponse object
+                ImageSearchResponse imageSearchResponse = JsonUtils.parseJson(networkResponse, ImageSearchResponse.class);
 
-                        callbackListener.imageSearchResponseReceived(newGoogleImageList);
-                    }
+                // if this is a "new" request (first request for an image) create a new GoogleImageList object
+                if (googleImageList == null) {
+                    Log.i(TAG, "fetchImages - new GoogleImageList object created");
+
+                    callbackListener.imageSearchResponseReceived(new GoogleImageList(imageSearchResponse));
+                } else {
+                    Log.i(TAG, "fetchImages - appending result to existing GoogleImageList");
+
+                    // otherwise, append the current result to the existing GoogleImageList and return that
+                    GoogleImageList newGoogleImageList = googleImageList;
+                    newGoogleImageList.appendImageUrls(imageSearchResponse);
+
+                    callbackListener.imageSearchResponseReceived(newGoogleImageList);
                 }
             }
         });
--- app/src/main/java/mark/gimagesearch/utils/NetworkUtils.java
+++ app/src/main/java/mark/gimagesearch/utils/NetworkUtils.java
@@ -1,5 +1,12 @@
 package mark.gimagesearch.utils;
 
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
@@ -24,5 +31,33 @@ public class NetworkUtils {
 
     public static Future<?> runInThreadPool(Runnable runnable) {
         return getInstance().mThreadPoolExecutor.submit(runnable);
+    }
+
+    public static String callUrl(URL url) {
+        if(url == null) {
+            Log.e(TAG, "callUrl - null url; returning null string");
+            return null;
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            URLConnection connection = url.openConnection();
+
+            // TODO do we need this referer property?
+            connection.addRequestProperty("Referer", "www.mark.gg");
+
+            String line;
+            InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+        } catch(IOException exception) {
+            Log.e(TAG, "callUrl - exception thrown when calling url, " + url.toString() + ": " + exception);
+            return null;
+        }
+
+        return builder.toString();
     }
 }
