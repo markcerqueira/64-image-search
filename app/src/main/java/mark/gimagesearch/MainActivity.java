5490388fb8f02704b1467fece761874322d9b435
Mark Cerqueira <mark.cerqueira@gmail.com>
2014-10-14 00:37:50 -0700

    Add Android Annotations

--- app/src/main/java/mark/gimagesearch/MainActivity.java
+++ app/src/main/java/mark/gimagesearch/MainActivity.java
@@ -8,13 +8,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.SearchView;
 
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.main_activity)
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getName();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.main_activity);
     }
 
     private void fetchImages(final String searchQuery) {
